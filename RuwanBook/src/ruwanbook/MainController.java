54853f9557365303c61969053b519ea70b9f25e4
Rukshani Somarathna <124167R@uom.lk>
2015-08-12 01:05:31 +0530

    sql

--- RuwanBook/src/ruwanbook/MainController.java
+++ RuwanBook/src/ruwanbook/MainController.java
@@ -6,10 +6,13 @@ package ruwanbook;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -31,9 +34,28 @@ public class MainController implements Initializable {
     
     @FXML
     protected void handleSubmitButtonAction() throws IOException{
+        connect con=new connect();
+        try {
+//            con.createTableCustomer();
+//            con.createTableEmployee();
+//            con.createTableSupplier();
+//            con.createTableProduct();
+            
+//            con.insertEmployee("r", "r", "rukshani","kuru", 037);
+//            con.insertEmployee("ra", "ra", "ravishani","kuru", 0372);
+//            con.insertEmployee("nila", "nila", "nilakshika","kuru", 0372);
+//            con.insertEmployee("NILA", "nila", "nilakshika","kuru", 0372);
+            
+           con.deleteEmployee("NILA");
+            
+            
+            
+        } catch (Exception ex) {
+            System.out.println("Exception "+ex);
+        }
         
         try {
-            Statement st = connection.getcon().createStatement();
+            Statement st = connect.getcon().createStatement();
             ResultSet rs=st.executeQuery("select * from EMPLOYEE");
             String user=usernameField.getText();
             String pass=passwordField.getText();
@@ -44,7 +66,7 @@ public class MainController implements Initializable {
             while (rs.next()) {
                 passdb=rs.getString("PASSWORD");
                 userdb=rs.getString("USERNAME");
-                System.out.println(passdb+" "+userdb);
+                System.out.println(userdb+" "+passdb);
 
                 if (pass.equals(passdb)&& user.equals(userdb)) { 
                     actiontarget.setText("UserName and Password correct");                   
---
+++ RuwanBook/src/ruwanbook/connect.java
@@ -0,0 +1,95 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ruwanbook;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author rukshani
+ */
+public class connect {
+    private static final String customerTable = "customer";
+    private static final String employeeTable = "employee";
+    private static final String productTable = "product";
+    private static final String supplierTable = "supplier";
+    
+     public static Connection getcon(){
+        try {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+            //DriverManager.registerDriver(new org.apache.derby.jdbc.EmbeddedDriver());
+            System.out.println("ok1");
+            String dbURL = "jdbc:derby://localhost:1527/Inventory;create=true";
+            //Connect and create a database if not exist in the memory:
+            String user = "APP";
+            String password = "i";
+            Connection con = DriverManager.getConnection(dbURL,user,password);
+            System.out.println("ok2");
+            return con;
+            
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("not ok "+ex);
+        }
+        return null;        
+    }
+//    public void createTableCustomer() throws SQLException {
+//        Statement st = connect.getcon().createStatement();
+//        st.execute("CREATE TABLE "
+//          + customerTable
+//          + "(id INTEGER NOT NULL PRIMARY KEY, name VARCHAR(25) NOT NULL,address VARCHAR(50), phone INTEGER)");
+//        System.out.println("Created table "+ customerTable);
+//    }
+//    public void createTableEmployee() throws SQLException {//        
+//        Statement st = connect.getcon().createStatement();
+//        st.execute("CREATE TABLE "
+//          + employeeTable
+//          + "(username VARCHAR(25) NOT NULL PRIMARY KEY, password VARCHAR(25),name VARCHAR(25),address VARCHAR(50), phone INTEGER)");
+//        System.out.println("Created table "+ employeeTable);
+//    }
+      public void insertEmployee(String username,String password,String name,String address,int phone){
+        try {
+            Statement st = connect.getcon().createStatement();
+            String sqlins="INSERT INTO "+employeeTable+" VALUES(\'"+username+"\',\'"+password+"\',\'"+name+"\',\'"+address+"\',"+phone+")";
+            st.execute(sqlins);
+            st.close();
+        } catch (SQLException ex) {
+            System.out.println("Insert error "+ employeeTable+ ex);
+        }
+      }
+      public void deleteEmployee(String username){
+        try {
+            Statement ps=connect.getcon().createStatement();
+            String sqldel="DELETE FROM "+employeeTable+" WHERE username=\'"+username+"\'";
+            ps.execute(sqldel);
+        } catch (SQLException ex) {
+            System.out.println("Delete error "+ employeeTable+ ex);
+        }          
+      }
+      
+//    public void createTableSupplier() throws SQLException {
+//        System.out.println("fakfjqjfa");
+//        Statement st = connect.getcon().createStatement();
+//        st.execute("CREATE TABLE "
+//          + supplierTable
+//          + "(id INTEGER NOT NULL PRIMARY KEY, name VARCHAR(25), address VARCHAR(50), phone INTEGER,company VARCHAR(25))");
+//        System.out.println("Created table "+ supplierTable);
+//    }
+//    public void createTableProduct() throws SQLException {
+//        Statement st = connect.getcon().createStatement();
+//        st.execute("CREATE TABLE "
+//          + productTable
+//          + "(serialnumber INTEGER NOT NULL PRIMARY KEY, name VARCHAR(25), description VARCHAR(75), price INTEGER,quantity INTEGER, category VARCHAR(25), brand VARCHAR(25),totalprice INTEGER, supplierId INTEGER, stocklevel INTEGER, imageurl VARCHAR(75),"
+//                + "FOREIGN KEY(supplierId) REFERENCES supplier(id))");
+//        System.out.println("Created table "+ productTable);
+//    }
+    
+}
